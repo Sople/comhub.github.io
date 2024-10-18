b864854b4086bc61f565fc2738fd5f677606072d
Cody.Liu <22427922+iwwwto@users.noreply.github.com>
2024-10-18 10:24:47 +0800

    Update title

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -52,7 +52,7 @@ const sidebarConfig = getSidebar(path.resolve(__dirname, '../docs')).reduce((acc
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "WWWTO - find your way online",
+  title: "wwwto - find your way online",
   description: "Full-Stack Development Technologies",
   head: [
     ['link', { rel: 'icon', href: '/logo/favicon3.svg' }], // 引用 favicon.icoo
